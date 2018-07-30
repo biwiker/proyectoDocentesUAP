2fc79bc8755e9962ce80e34f0c3a2bd63f2cee64
biwiker2018 <41716561+biwiker@users.noreply.github.com>
2018-07-29 23:32:12 -0400

    Interfáz de login (Eliseo)

--- capaPresentacion/V_Administrador.php
+++ capaPresentacion/V_Administrador.php
@@ -3,7 +3,7 @@
         <title>Vista_Administrador</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <link href="../bootstrap 3/css/bootstrap.css" rel="stylesheet" type="text/css">
-        <link href="../Css/Estilos_Admin.css" rel="stylesheet" type="text/css">
+        <link href="../css/Estilos_Admin.css" rel="stylesheet" type="text/css">
         <script src="../bootstrap 3/js/bootstrap.js"></script>
         <script src="../bootstrap 3/jquery-3.3.1.js"></script>
     </head>
--- capaPresentacion/V_Login.php
+++ capaPresentacion/V_Login.php
@@ -3,7 +3,7 @@
         <title>Login</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <link href="../bootstrap 3/css/bootstrap.css" rel="stylesheet" type="text/css">
-        <link href="../Css/Estilos_Login.css" rel="stylesheet" type="text/css">
+        <link href="../css/Estilos_Login.css" rel="stylesheet" type="text/css">
         <script src="../bootstrap 3/js/bootstrap.js"></script>
         <script src="../bootstrap 3/jquery-3.3.1.js"></script>
     </head>
