2171588dd01c91497b5262a1342446d49e4c4277
biwiker2018 <41716561+biwiker@users.noreply.github.com>
2018-07-31 00:12:44 -0400

    Trabajando con V_Admin

--- capaPresentacion/V_Administrador.php
+++ capaPresentacion/V_Administrador.php
@@ -8,20 +8,26 @@
         <script src="../bootstrap 3/jquery-3.3.1.js"></script>
     </head>
     <body>
+        <!--este div contiene el color del sistema-->
         <div class="contenedor"> 
+            <!-- div de la barra superior -->
         <header class="cabezera"> 
+            <!--imagen duoc uc-->
             <img src="../Imagenes/fondo-transparente_1.png" height="50px" width="200px">
+            <!--texto donde deberia aparecer el usuario que inició sesion-->
             <p>Bienvenido/a </p>
+            <!--boton que destruye sesion-->
             <button class="boton_personalizado" name="btnCerrarSesion" value="Cerrar Sesion"><span class="glyphicon glyphicon-remove"></span> Cerrar Sesion</button>
         </header>
+           
             <div class="articulo_1">
                 <h1><span class="glyphicon glyphicon-ok"></span> Administrador Del Sistema</h1>
             </div>
             <div class="articulo_2">
                 <label id="Docente">Rut Docente</label>
-                <input type="text" placeholder="Ingrese Rut" name="txtRut" id="Rut">
+                <input type="text" placeholder="Ingrese Rut" name="txtRut" id="Rut" required >
                 <label id="Escuela">Escuela</label>
-                <input type="text" placeholder="Ingrese Escuela" name="txtEscuela" id="txtEscuela"> 
+                <input type="text" placeholder="Ingrese Escuela" name="txtEscuela" id="txtEscuela" required> 
                 <button class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off" name="btnVerDocente" value="Ver Docente" >Ver
                 </button>
             </div>
