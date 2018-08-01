3b48ce0f367c9358851e44e161abc09e76799d53
biwiker2018 <41716561+biwiker@users.noreply.github.com>
2018-07-31 23:25:52 -0400

    Trabajando con V_Admin

--- capaPresentacion/V_Administrador.php
+++ capaPresentacion/V_Administrador.php
@@ -20,26 +20,29 @@
             <button class="boton_personalizado" name="btnCerrarSesion" value="Cerrar Sesion"><span class="glyphicon glyphicon-remove"></span> Cerrar Sesion</button>
         </header>
             <!--div que muestra "adminstrador del sistema"-->
-            <div class="articulo_1">
+            <div class="articulo_1 ">
                 <h1><span class="glyphicon glyphicon-ok"></span> Administrador Del Sistema</h1>
             </div>
-            <!--div que muestra las cajas de texto y el boton-->
+            <!--div que muestra las cajas de texto y los botones-->
             <div class="articulo_2">
                 <label id="Docente">Rut Docente</label>
                 <input type="text" placeholder="Ingrese Rut" name="txtRut" id="Rut" required >
+                
+                <button class="btn btn-primary" data-toggle="button" aria-pressed="false" id="btnVerDocente"  autocomplete="off" name="btnVerDocente" value="VerDocente" >Ver Docente</button>
+                
                 <label id="Escuela">Escuela</label>
                 <input type="text" placeholder="Ingrese Escuela" name="txtEscuela" id="txtEscuela" required> 
-                <button class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off" name="btnVerDocente" value="Ver Docente" >Ver
-                </button>
+               
+                <button class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off"  id="btnVerEscuela" name="btnVerEscuela" value="VerEscuela" >Ver Escuela</button>
             </div>
             <!--div que muestra la otra funcion que tiene el administrador-->
             <div class="articulo_3">
-                <h4>Inclusion</h4>
-                <button class="btn btn-success" id="btnEditarInclusion" name="btnEditarInclusion" value="Ver Inclusion">Ver</button>      
+                <h4>Informacion Docente</h4>
+                <button class="btn btn-success" id="btnEditarInclusion" name="btnInfoDocente" value="VerInfoDocente">Ver</button>      
             </div>
             <div class="articulo_4">
                 <h4>Uap</h4>
-                <button class="btn btn-success" id="btnEditarUap" name="btnEditarUap" value="Ver Uap">Ver</button>
+                <button class="btn btn-success" id="btnEditarUap" name="btnEditarUap" value="VerUap">Ver</button>
             </div>
        </div>
     </body>
