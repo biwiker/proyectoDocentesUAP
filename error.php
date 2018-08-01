888b680807cce555f95ad31fed44e05d08ef7209
biwiker2018 <41716561+biwiker@users.noreply.github.com>
2018-07-31 22:38:22 -0400

    modificacion a pagina de error (Adolfo)

--- error.php
+++ error.php
@@ -1,16 +1,48 @@
-<?php
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    </head>
+    <title>Error 404</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link href="css/Estilos_Login.css" rel="stylesheet" type="text/css">
+        <link href="bootstrap 3/css/bootstrap.css" rel="stylesheet" type="text/css">
+            <script src="bootstrap 3/js/bootstrap.js"></script>
+            <script src="bootstrap 3/jquery-3.3.1.js"></script>
+            
+            <body style="background-color: ">
+                <!--cabecera con imagen de DUOC-->
+                <header style="width: 100%;top: 1px;left: 0px;padding-top: 15px; background-color: #001e39; position: relative">
+                    <img src="Imagenes/fondo-transparente_4.png"> 
+                </header>
+                <!--linea de división del header amarilla-->
+                <hr style="height: 7px; background-color: #FACC2E; margin-top: 0;margin-bottom: 60px; box-shadow: 0 3px 3px #ccc">
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+                <!--imagen de error en el lado izquierdo-->
+                    <div>
+                        <div class="col-md-4"> 
+                            <img src="Imagenes/error.png" class="col-md-4" style="position: relative; display: block; left: 1%">
+                        </div>
+                <!--mensaje de error al pie de pagina lado derecho-->        
+                        <div>
+                            <h1 class="alert alert-danger" style="position: absolute; right: 50%"><strong>ERROR 404 NO FOUND</strong></h1>
+                        </div>
+                    </div>
 
-/**
- * Description of error
- *
- * @author adolfo
- */
-class error {
-    //put your code here
-}
+
+                    <!--
+                    <div>
+                        <h1 class="alert-danger"><strong>ERROR 404 NO FOUND</strong></h1>
+                    </div>
+
+                     <div class="alert alert-warning">
+                        <strong>ERROR 404 NO FOUND</strong> Lo sentimos no encontramos la pagina.
+                    </div>-->
+            </body>
+            <footer style="position: absolute; bottom: 5px; right: 10px">
+                <div class="alert alert-danger">
+                    <a href="capaPresentacion/V_Login.php" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                    <strong>Lo sentimos no encontramos la pagina</strong>.
+                </div>
+            </footer>
+            </html>
