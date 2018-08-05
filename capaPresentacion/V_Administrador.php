97801931f7e2fa7a742a93dc27eddf386b94a716
biwiker2018 <41716561+biwiker@users.noreply.github.com>
2018-08-05 01:12:27 -0400

    Actualización del sitio de administrador. Se corrigen elementos gráficas ajustables a resolución de 1024 x 768. Se agregar elementos desplegables con botones que mediante css permiten ocultar y desplegar contenido, además de utilizar las fuentes para cargar el los símboles "+" y "-"

--- capaPresentacion/V_Administrador.php
+++ capaPresentacion/V_Administrador.php
@@ -4,46 +4,129 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <link href="../bootstrap 3/css/bootstrap.css" rel="stylesheet" type="text/css">
         <link href="../css/Estilos_Admin.css" rel="stylesheet" type="text/css">
-        <script src="../bootstrap 3/js/bootstrap.js"></script>
-        <script src="../bootstrap 3/jquery-3.3.1.js"></script>
+        <!--  bajar versión local-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        
     </head>
     <body>
-        <!--este div contiene el color del sistema-->
-        <div class="contenedor"> 
-            <!-- div de la barra superior -->
-        <header class="cabezera"> 
+
+        <!-- div de la barra superior -->
+        <header class="principal"> 
             <!--imagen duoc uc-->
-            <img src="../Imagenes/fondo-transparente_1.png" height="50px" width="200px" >
-            <!--Mostrar quien es el usuario ingresado en el sistema(Revisar)-->
-            <p class="text-warning">Bienvenido/a: <?php echo $_SESSION['nombre']; ?> <?php echo $_SESSION['apellido']; ?></p>
-            <!--boton que destruye sesion-->
-            <button class="boton_personalizado" name="btnCerrarSesion" value="Cerrar Sesion"><span class="glyphicon glyphicon-remove"></span> Cerrar Sesion</button>
+            <div class="logo">
+                <img src="../Imagenes/fondo-transparente_1.png">
+            </div>
+            <div class="info-usuario">
+                <div>
+                    <!--Mostrar quien es el usuario ingresado en el sistema-->
+                    <p>Bienvenido/a a <strong>Duoc UC</strong>: Carolina de la Cerda <?php echo @$_SESSION['nombre']; ?> <?php echo @$_SESSION['apellido']; ?></p>
+                </div>
+                <div>
+                    <!--boton que destruye sesion-->
+                    <button class="boton_personalizado" name="btnCerrarSesion" value="Cerrar Sesion"><span class="glyphicon glyphicon-remove"></span> Cerrar Sesion</button>
+                </div>
+            </div>
+
+
+
         </header>
+        <hr>
+        <!--este div contiene el color del sistema-->
+        <div class="contenedor">    
+
             <!--div que muestra "adminstrador del sistema"-->
-            <div class="articulo_1 ">
-                <h1><span class="glyphicon glyphicon-ok"></span> Administrador Del Sistema</h1>
+            <div class="articulo_1">
+                <h5><span class="glyphicon glyphicon-ok"></span> Administrador Del Sistema</h5>
             </div>
             <!--div que muestra las cajas de texto y los botones-->
-            <div class="articulo_2">
-                <label id="Docente" class="label label-default">Rut Docente</label>
-                <input type="text" placeholder="Ingrese Rut" name="txtRut" id="Rut" required >
+            <article class="articulo_2">
+                <section>
+                    <label id="lblDocente" class="label label-default">Rut Docente</label>
+                    <input type="text" placeholder="Ingrese Rut" name="txtRut" id="txtRut" required >
+                    <button class="btn" data-toggle="button" aria-pressed="false" id="btnVerDocente"  autocomplete="off" name="btnVerDocente" value="VerDocente" >Ver Docente</button>
+                </section>
+
+                <section class="art2-sec-2">
+                    <label id="lblEscuela" class="label label-default">Escuela</label>
+                    <input type="text" placeholder="Ingrese Escuela" name="txtEscuela" id="txtEscuela" required> 
+                    <button class="btn" data-toggle="button" aria-pressed="false" autocomplete="off"  id="btnVerEscuela" name="btnVerEscuela" value="VerEscuela" >Ver Escuela</button>
+                </section>
+
+            </article>
+            <!-- articulo 3-->
+            <article class="articulo_3">
                 
-                <button class="btn btn-primary" data-toggle="button" aria-pressed="false" id="btnVerDocente"  autocomplete="off" name="btnVerDocente" value="VerDocente" >Ver Docente</button>
+                <article>
+                    <section>
+                        
+                            <h4>Informacion Docente</h4>
+                            <span data-toggle="collapse"  aria-expanded="false" href="#art3-cargaDatos">
+                                
+                                <button class="btn" id="btnInfoDocente" name="btnInfoDocente" value="btnInfoDocente"><span></span></button>
+                            </span>
+                            <!--boton-->
+                            
+                            <!--linea de division-->
+                            <div id="linea-section"></div>
+                    </section>
+                    <!--esta sección contiene la información-->
+                    <section id="art3-cargaDatos" class="collapse">
+                        <div class="panel-body">
+                            contenido desde ajax
+                        </div>
+                    </section>
+                </article>
+            </article>
+            <!-- articulo 4-->
+            <article class="articulo_3">
                 
-                <label id="Escuela" class="label label-default">Escuela</label>
-                <input type="text" placeholder="Ingrese Escuela" name="txtEscuela" id="txtEscuela" required> 
-               
-                <button class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off"  id="btnVerEscuela" name="btnVerEscuela" value="VerEscuela" >Ver Escuela</button>
-            </div>
-            <!--div que muestra la otra funcion que tiene el administrador-->
-            <div class="articulo_3">
-                <h4>Informacion Docente</h4>
-                <button class="btn btn-success" id="btnEditarInclusion" name="btnInfoDocente" value="VerInfoDocente">Ver</button>      
-            </div>
-            <div class="articulo_4">
-                <h4>Uap</h4>
-                <button class="btn btn-success" id="btnEditarUap" name="btnEditarUap" value="VerUap">Ver</button>
-            </div>
-       </div>
+                <article>
+                    <section>
+                        
+                            <h4>Informacion Docente</h4>
+                            <span data-toggle="collapse"  aria-expanded="false" href="#art4-cargaDatos">
+                                
+                                <button class="btn" id="btnInfoDocente" name="btnInfoDocente" value="btnInfoDocente"><span></span></button>
+                            </span>
+                            <!--boton-->
+                            
+                            <!--linea de division-->
+                            <div id="linea-section"></div>
+                    </section>
+                    <!--esta sección contiene la información-->
+                    <section id="art4-cargaDatos" class="collapse">
+                        <div class="panel-body">
+                            contenido desde ajax
+                        </div>
+                    </section>
+                </article>
+            </article>
+            <!-- articulo 5-->
+            <article class="articulo_3">
+                
+                <article>
+                    <section>
+                        
+                            <h4>Informacion Docente</h4>
+                            <span data-toggle="collapse"  aria-expanded="false" href="#art5-cargaDatos">
+                                
+                                <button class="btn" id="btnInfoDocente" name="btnInfoDocente" value="btnInfoDocente"><span></span></button>
+                            </span>
+                            <!--boton-->
+                            
+                            <!--linea de division-->
+                            <div id="linea-section"></div>
+                    </section>
+                    <!--esta sección contiene la información-->
+                    <section id="art5-cargaDatos" class="collapse">
+                        <div class="panel-body">
+                            contenido desde ajax
+                        </div>
+                    </section>
+                </article>
+            </article>
+            
+        </div>
     </body>
 </html>
