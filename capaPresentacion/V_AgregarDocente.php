2694daa28d283ac0d4a0c827a6e629e8d6806f1c
biwiker2018 <41716561+biwiker@users.noreply.github.com>
2018-08-17 00:14:35 -0300

    Trabajando en pagina agregar docente

--- capaPresentacion/V_AgregarDocente.php
+++ capaPresentacion/V_AgregarDocente.php
@@ -16,20 +16,6 @@
                 var fila1 = row.insertCell(0);
                 fila1.innerHTML = '<table class=" table-hover  table-condensed table-responsive" id="myTable"> \n\
                         '+'<tr> \n\
-                        '+'<th>Rut</th> \n\
-                        '+'<th>DV</th> \n\
-                        '+'<th>ID Docente</th> \n\
-                        '+'<th>Primer Nombre</th> \n\
-                        '+'<th>Segundo Nombre</th> \n\
-                        '+'<th>Apellido Paterno</th> \n\
-                        '+'<th>Apellido Materno</th> \n\
-                        '+'<th>ID CentroCosto</th> \n\
-                        '+'<th>Correo Duoc</th> \n\
-                        '+'<th>Correo Personal</th> \n\
-                        '+'<th>Telefono Fijo</th> \n\
-                        '+'<th>Telefono Movil</th> \n\
-                        '+'</tr> \n\
-                        '+'<tr> \n\
                         '+'<td><input type="text" class="form-control"> </td> \n\
                         '+'<td><input type="text" class="form-control"> </td> \n\
                         '+'<td><input type="text" class="form-control"> </td> \n\
@@ -54,34 +40,59 @@
             }
           </script>
     </head>
-
-    <body>
-        <div class="row">
-            <div class="col-lg-4 col-md-5 col-xs-6">
-                <img src="../Imagenes/fondo-transparente_3.png" title="Logo DuocUC" class="Logo img-responsive">
+    <body>   
+            <div id="sidebar" class="sidebar">
+                <ul class="menu">
+                    <li><a href="#"><span class="glyphicon glyphicon-th-large"></span><p>Administrar</p></a></li>
+                    <li><a href="#"><span class="glyphicon glyphicon-signal"></span><p>Estad&iacute;sticas</p></a></li>
+                    <li><a href="#"><span class="glyphicon glyphicon-user"></span><p>Usuarios</p></a></li>
+                    <li><a href="#"><span class="glyphicon glyphicon-cog"></span><p>Configuraci&oacute;n</p></a></li>
+                </ul>
             </div>
-            <div class="col-lg-8 col-xs-6">
-                <p class="sesion">Bienvenido/a a <strong>Duoc UC</strong>:</p>
+            <div id="contenido-total">
+              <header class="principal"> 
+                    <!--imagen duoc uc-->
+                    <div class="logo">
+                        <img src="../Imagenes/fondo-transparente_4.png">
+                    </div>
+                    <div class="info-usuario">
+                        <div>
+                            <!--Mostrar quien es el usuario ingresado en el sistema-->
+                            <p>Bienvenido/a a <strong>Duoc UC</strong>:</p>
+                        </div>
+                        <div>
+                            <!--boton que destruye sesion-->
+                            <form method="POST" action="../capaLogicaNegocio/BO_Accesos.php">
+                                <button class="boton_personalizado" name="btnCerrarSesion" value="Cerrar Sesion">
+                                    <span class="glyphicon glyphicon-remove"></span> Cerrar Sesion
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </header>
             </div>
-        </div>
-    <hr>
-        <div class="col-lg-12 articulo_1">
-            <ul>
-                <li><a href="#">Docentes</a></li>
-                <li><a href="#">Adminstrar</a></li>
-            </ul>
-        </div>
-    <!--boton que tiene la funcion de agregar una nueva columna a la tabla-->
-    <div class="btn btn-success" onclick="agregarFila()" id="btnNuevo"><span class="glyphicon glyphicon-file"></span> Nuevo Docente </div>
-    <div class="form-group">
+        <hr>
+        <div class="articulo_1"> 
             <div class="row">
                 <div class="col-lg-12">
-                <table id="myTable">
-                    
-                </table>
-               </div> 
+                     <!--boton que tiene la funcion de agregar una nueva columna a la tabla-->
+                     <div class="btn btn-success" id="btnNuevo"><span class="glyphicon glyphicon-file"></span> Nuevo Docente </div>
+                     <!--etiqueta-->
+                     <label class=" label label-default">Filtros De Busqueda</label>
+                     <!--caja de texto para el rut-->
+                     <input type="text" class="txtRut" placeholder="Ingrese Rut" >
+                     <!--select que se debe relacionar con la base de datos para obtener las escuelas-->
+                         <div class="caja">
+                          <select>
+                           <option>PUENTE ALTO</option>
+                         </select>
+                    </div>
+                </div>
             </div>
-        </div>
+
+        </div>  
+    
+    
          <!-- <div class="btn btn-primary" id="btnGuardar"><span class="glyphicon glyphicon-edit"></span> Guardar</div>
           <div class="btn btn-danger" id="btnEliminar"><span class="glyphicon glyphicon-remove"></span> Eliminar</div>-->
 </body>
